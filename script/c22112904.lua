eafdada38dd0987067d2d9af975e88a2febdccb2
Clara <lyris89@live.com>
2020-06-22 09:03:48 -0400

    Update progress
    Add:
    * "Fortitude of the most Valiant Souls"
    * "Valiant Soul Priestess - Yuriko"
    * "Secret of the Valiant Souls"
    * "Valiant Soul Abyssqueen - Aika"
    * "Valiant Soul Darkness Swordswitch"
    Fix "Valiant Soul Princess - Tsukiko": Add category to 2nd effect

--- script/c22112904.lua
+++ script/c22112904.lua
@@ -1,7 +1,6 @@
---Valiant Soul Princess - Tsukiko
-function c22112904.initial_effect(c)
-	--You can only use each effect of "Valiant Soul Princess - Tsukiko" once per turn.
-	--If another card in your possession is destroyed by the effect of a "Valiant Soul" card: you can Special Summon this card from your hand.
+--created by Geass, coded by Lyris
+local cid,id=GetID()
+function cid.initial_effect(c)
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
 	e1:SetCode(EVENT_DESTROYED)
@@ -13,21 +12,21 @@ function c22112904.initial_effect(c)
 	e1:SetTarget(cid.sptg)
 	e1:SetOperation(cid.spop)
 	c:RegisterEffect(e1)
-	--You can target 1 other "Valiant Soul" monster you control; its Attribute becomes WATER, also increase its Level by 1.
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_IGNITION)
 	e2:SetRange(LOCATION_MZONE)
 	e2:SetCountLimit(1,id+100)
 	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e2:SetCategory(CATEGORY_LVCHANGE)
 	e2:SetTarget(cid.target)
 	e2:SetOperation(cid.operation)
 	c:RegisterEffect(e2)
 end
-function c3113667.cfilter(c,tp)
+function cid.cfilter(c,tp)
 	return c:IsReason(REASON_EFFECT) and c:IsPreviousLocation(LOCATION_ONFIELD) and c:GetPreviousControler()==tp
 end
-function c3113667.condition(e,tp,eg,ep,ev,re,r,rp)
-	return re and re:GetHandler():IsSetCard(0xa88) and eg:IsExists(c3113667.cfilter,1,e:GetHandler(),tp)
+function cid.condition(e,tp,eg,ep,ev,re,r,rp)
+	return re and re:GetHandler():IsSetCard(0xa88) and eg:IsExists(cid.cfilter,1,e:GetHandler(),tp)
 end
 function cid.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	local c=e:GetHandler()
