b6d6f174ef01f744f6d9821261c77ed6bce39817
Clara <lyris89@live.com>
2020-06-25 07:01:02 -0400

    Update progress
    Fix "Valiant Soul" cards:
    * "Hiroko" revival effect now actually targets
    * "Cardinal Ruler":
      * Add descriptions to effects
      * Now properly adds from either GY to the hand
    Add "Wondrous Realm" cards:
    * Field Spell
    * "Rabbit Hole"

---
+++ script/c19231213.lua
@@ -0,0 +1,54 @@
+--Rabbit Hole to the Wondrous Realm
+function c19231213.initial_effect(c)
+	--You can only activate 1 "Rabbit Hole to the Wondrous Realm" effect per turn, and only once that turn.
+	--Toss a coin.
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_COIN)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCountLimit(1,id)
+	e1:SetTarget(c37812118.target)
+	e1:SetOperation(c37812118.activate)
+	c:RegisterEffect(e1)
+	--During your Main Phase: You can banish this card from your GY: Special Summon 1 "Wondrous Realm" monster from your GY.
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_QUICK_O)
+	e2:SetCode(EVENT_FREE_CHAIN)
+	e2:SetRange(LOCATION_GRAVE)
+	e2:SetCountLimit(1,id)
+	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e2:SetCondition(function(e,tp) local ph=Duel.GetCurrentPhase() return Duel.GetTurnPlayer()==tp and (ph==PHASE_MAIN1 or ph==PHASE_MAIN2) end)
+	e2:SetCost(aux.bfgcost)
+	e2:SetTarget(cid.sptg)
+	e2:SetOperation(cid.spop)
+	c:RegisterEffect(e2)
+end
+function cid.filter(c,e,tp)
+	return (c:IsFaceup() and Duel.GetLocationCountFromEx(tp)>0 or not c:IsLocation(LOCATION_EXTRA) and Duel.GetLocationCount(tp,LOCATION_MZONE)>0)
+		and c:IsType(TYPE_PENDULUM) and c:IsSetCard(0xa44) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function cid.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(cid.filter,tp,LOCATION_EXTRA,0,1,nil,e,tp)
+		and Duel.IsExistingMatchingCard(cid.filter,tp,LOCATION_HAND,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_COIN,nil,0,tp,1)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,0)
+end
+function cid.activate(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	--Heads: Special Summon 1 face-up "Wondrous Realm" Pendulum Monster from your Extra Deck.
+	--Tails: Special Summon 1 "Wondrous Realm" Pendulum Monster from your hand.
+	Duel.SpecialSummon(Duel.SelectMatchingCard(tp,cid.filter,tp,Duel.TossCoin(tp,1)==1 and LOCATION_EXTRA or LOCATION_HAND,0,1,1,nil,e,tp),0,tp,tp,false,false,POS_FACEUP)
+end
+function cid.spfilter(c,e,tp)
+	return c:IsSetCard(0xa44) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function cid.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(cid.spfilter,tp,LOCATION_GRAVE,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_GRAVE)
+end
+function cid.spop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	Duel.SpecialSummon(Duel.SelectMatchingCard(tp,cid.spfilter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp),0,tp,tp,false,false,POS_FACEUP)
+end
