9ca19c1b941c018d5000799566f0eab480ddf315
Lyris12 <lyris89@live.com>
2022-05-20 09:11:38 -0400

    cleanup placeholder scripts

--- script/c85120907.lua
+++ script/c85120907.lua
@@ -2,7 +2,8 @@
 --Cosmic Conversion
 local s,id,o=GetID()
 function s.initial_effect(c)
-	local tp=c:GetControler()	local e1=Effect.CreateEffect(c)
+	local tp=c:GetControler()
+	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
 	e1:SetCode(EVENT_PHASE_START+PHASE_DRAW)
 	e1:SetCountLimit(1,5001+EFFECT_COUNT_CODE_DUEL)
