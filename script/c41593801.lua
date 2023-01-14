5730359dfbfc117cf9c7bb60994e41fdb53d72d5
Lyris12 <Lyris89@live.com>
2023-01-14 03:29:54 -0500

    start "Skypiercer" scripts

--- script/c41593801.lua
+++ script/c41593801.lua
@@ -2,15 +2,68 @@
 --Skypiercer BF-110
 local s,id,o=GetID()
 function s.initial_effect(c)
+	-- Unaffected by your opponent's monster effects the turn it is Special Summoned
+	local e1 = Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCode(EFFECT_IMMUNE_EFFECT)
+	e1:SetValue(function(e, te) return te:GetOwnerPlayer()~=e:GetHandlerPlayer() and te:IsActiveType(TYPE_MONSTER) end)
+	e1:SetCondition(function() return c:IsStatus(STATUS_SPSUMMON_TURN) end)
+	c:RegisterEffect(e1)
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetCode(EFFECT_SPSUMMON_PROC)
+	e2:SetRange(LOCATION_HAND)
+	e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e2:SetCondition(s.spcon)
+	e2:SetOperation(s.spop)
+	c:RegisterEffect(e2)
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_IGNITION)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetCountLimit(1,id)
+	e3:SetCategory(CATEGORY_SEARCH+CATEGORY_TOHAND+CATEGORY_DRAW)
+	e3:SetCost(s.thcost)
+	e3:SetTarget(s.thtg)
+	e3:SetOperation(s.thop)
+	c:RegisterEffect(e3)
+end
+-- Check if the monster can be Special Summoned from the hand by discarding another "Skypiercer" card
+function s.cfilter(c)
+	return c:IsSetCard(0x3bb) and c:IsDiscardable()
+end
+function s.spcon(e, c)
+	if c==nil then return true end
 	local tp=c:GetControler()
-	local e0=Effect.CreateEffect(c)
-	e0:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-	e0:SetCode(EVENT_PHASE_START+PHASE_DRAW)
-	e0:SetCountLimit(1,5001+EFFECT_COUNT_CODE_DUEL)
-	e0:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
-	e0:SetOperation(function()
-		local tk=Duel.CreateToken(tp,5000)
-		Duel.SendtoDeck(tk,nil,SEQ_DECKBOTTOM,REASON_RULE)
-	end)
-	Duel.RegisterEffect(e0,tp)
+	return Duel.IsExistingMatchingCard(s.cfilter, tp, LOCATION_HAND, 0, 1, c)
+end
+function s.spop(e, tp, eg, ep, ev, re, r, rp, c)
+	Duel.DiscardHand(tp, s.cfilter, 1, 1, REASON_DISCARD+REASON_COST, c)
+end
+-- Add a "Skypiercer" card from the Deck to the hand
+function s.thcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.cfilter, tp, LOCATION_HAND, 0, 1, nil) end
+	Duel.DiscardHand(tp, s.cfilter, 1, 1, REASON_DISCARD+REASON_COST)
+end
+function s.filter(c)
+	return c:IsSetCard(0x3bb) and c:IsAbleToHand()
+end
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.filter, tp, LOCATION_DECK, 0, 1, nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function s.dfilter(c)
+	return c:IsFaceup() and c:IsSetCard(0x3bb)
+end
+function s.thop(e, tp)
+	Duel.Hint(HINT_SELECTMSG, tp, HINTMSG_ATOHAND)
+	local g = Duel.SelectMatchingCard(tp, s.filter, tp, LOCATION_DECK, 0, 1, 1, nil)
+	Duel.SendtoHand(g, nil, REASON_EFFECT)
+	Duel.ConfirmCards(1-tp, g)
+	local tc = g:GetFirst()
+	-- If you control another "Skypiercer" monster, draw 1 card
+	if tc and tc:IsLocation(LOCATION_HAND) and Duel.IsExistingMatchingCard(s.dfilter, tp, LOCATION_MZONE, 0, 1, e:GetHandler()) then
+		Duel.Draw(tp, 1, REASON_EFFECT)
+	end
 end
