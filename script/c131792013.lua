31ee7569bee3e28b95e335800c0c173470e8ada5
Lyris12 <Lyris89@live.com>
2023-06-24 19:25:06 -0400

    update
    Start over on "Voidictator" scripts

--- script/c131792013.lua
+++ script/c131792013.lua
@@ -2,16 +2,18 @@
 --Radiant Concentrated Magitate
 local s,id,o=GetID()
 function s.initial_effect(c)
+	--1 Level 4 or lower non-WIND "Magitate" monster If a Level 5 "Magitate" monster is destroyed: You can return this banished card to the Extra Deck; Special Summon 1 "Magitate" monster from your GY, then transform it to [REVERSE] side. You can only use this effect of "Radiant Concentrated Magitate" once per turn.
 	local tp=c:GetControler()
-	local e0=Effect.CreateEffect(c)
-	e0:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-	e0:SetCode(EVENT_PHASE_START+PHASE_DRAW)
-	e0:SetCountLimit(1,5001+EFFECT_COUNT_CODE_DUEL)
-	e0:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
-	e0:SetOperation(function()
+	local ef=Effect.CreateEffect(c)
+	ef:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	ef:SetCode(EVENT_PHASE_START+PHASE_DRAW)
+	ef:SetCountLimit(1,5001+EFFECT_COUNT_CODE_DUEL)
+	ef:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
+	ef:SetOperation(function()
 		local tk=Duel.CreateToken(tp,5000)
 		Duel.SendtoDeck(tk,nil,SEQ_DECKBOTTOM,REASON_RULE)
-		c5000.ops(f0,tp)
+		c5000.ops(ef,tp)
 	end)
-	Duel.RegisterEffect(e0,tp)
+	Duel.RegisterEffect(ef,tp)
 end
+Card.IsConcentratedMagitate=Card.IsConcentratedMagitate or function(c) return c:GetCode()>131792009 and c:GetCode()<131792017 end
--- script/c131792015.lua
+++ script/c131792015.lua
@@ -2,16 +2,18 @@
 --On The Practical Applications of Concentrated Magitate
 local s,id,o=GetID()
 function s.initial_effect(c)
+	--Special Summon 1 "Magitate" monster from your Deck with the same attribute as a "Concentrated Magitate" monster you control. If this card is banished from your GY: You can return 1 card your opponent controls to the hand. You can only 1 effect of "On The Practical Applications of Concentrated Magitate" per turn, and only once that turn.
 	local tp=c:GetControler()
-	local e0=Effect.CreateEffect(c)
-	e0:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-	e0:SetCode(EVENT_PHASE_START+PHASE_DRAW)
-	e0:SetCountLimit(1,5001+EFFECT_COUNT_CODE_DUEL)
-	e0:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
-	e0:SetOperation(function()
+	local ef=Effect.CreateEffect(c)
+	ef:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	ef:SetCode(EVENT_PHASE_START+PHASE_DRAW)
+	ef:SetCountLimit(1,5001+EFFECT_COUNT_CODE_DUEL)
+	ef:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
+	ef:SetOperation(function()
 		local tk=Duel.CreateToken(tp,5000)
 		Duel.SendtoDeck(tk,nil,SEQ_DECKBOTTOM,REASON_RULE)
-		c5000.ops(f0,tp)
+		c5000.ops(ef,tp)
 	end)
-	Duel.RegisterEffect(e0,tp)
+	Duel.RegisterEffect(ef,tp)
 end
+Card.IsConcentratedMagitate=Card.IsConcentratedMagitate or function(c) return c:GetCode()>131792009 and c:GetCode()<131792017 end
