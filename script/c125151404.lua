2f828e917d9ee24c837e2e6d634471c8ce69af2f
Lyris12 <Lyris89@live.com>
2023-07-28 14:38:24 -0400

    update

--- script/c125151404.lua
+++ script/c125151404.lua
@@ -2,7 +2,7 @@
 --Exodice Fishe
 local s,id,o=GetID()
 function s.initial_effect(c)
-	--If this card leaves the field due to an opponent's card: Inflict 100 damage to your opponent, and if you do, Special Summon 1 "Exodice" monster from your Deck, except "Exodice Fishe".  If this card is Special Summoned from the Deck or banished: You can banish 1 Fish monster from your GY; Excavate the top 3 cards of your Deck, add 1 excavated Fish monster or "Exodice" card to your hand, also send the rest to the GY. You can only use this effect of "Exodice Fishe" once per turn.
+	--If this card leaves the field due to an opponent's card: Inflict 100 damage to your opponent, and if you do, Special Summon 1 "Exodice" monster from your Deck, except "Exodice Fishe". If this card is Special Summoned from the Deck or banished: You can banish 1 Fish monster from your GY; Excavate the top 3 cards of your Deck, add 1 excavated Fish monster or "Exodice" card to your hand, also send the rest to the GY. You can only use this effect of "Exodice Fishe" once per turn.
 	local tp=c:GetControler()
 	local ef=Effect.CreateEffect(c)
 	ef:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
--- script/c195208430.lua
+++ script/c195208430.lua
@@ -2,7 +2,7 @@
 --Mextro Sightseeker
 local s,id,o=GetID()
 function s.initial_effect(c)
-	--If this card is Normal or Special Summoned: Excavate the top 3 cards of your Deck, and if you do, you can add 1 excavated "Mextro" card to your hand, also shuffle the rest into your Deck, then, if the added card is a monster, you can Special Summon it. If this card is used as material for the Link Summon of a "Mextro" Link Monster: Gain 1000 LP, then you can activate 1 "Mextro" Field Spell from your  Deck or GY. You can only use each effect of "Mextro Sightseeker" once per turn.
+	--If this card is Normal or Special Summoned: Excavate the top 3 cards of your Deck, and if you do, you can add 1 excavated "Mextro" card to your hand, also shuffle the rest into your Deck, then, if the added card is a monster, you can Special Summon it. If this card is used as material for the Link Summon of a "Mextro" Link Monster: Gain 1000 LP, then you can activate 1 "Mextro" Field Spell from your Deck or GY. You can only use each effect of "Mextro Sightseeker" once per turn.
 	local tp=c:GetControler()
 	local ef=Effect.CreateEffect(c)
 	ef:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
