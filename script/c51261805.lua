e6ee6827bee67b3cc3191472ba28fa3e0495faa2
Lyris12 <lyris89@live.com>
2024-05-02 14:17:34 -0400

    add
    `Mextropolis City`
    `Skypiercer Zeppelin`
    `Elflair - Defensive Song`
    `Elflair - Irene, Wellspring Elf Princess`

--- script/c51261805.lua
+++ script/c51261805.lua
@@ -2,17 +2,60 @@
 --Elflair - Irene, Wellspring Elf Princess
 local s,id,o=GetID()
 function s.initial_effect(c)
-	--2 "Elflair" monsters This card can attack while in face-up Defense Position. If it does, apply its DEF for damage calculation. Must first be Special Summoned (from your Extra Deck) by sending the above cards from either field to the GY. (You do not use "Polymerization".) Once per turn, when a card or effect is activated (Quick Effect): You can remove 1 Elflair counter from anywhere on the field; negate the activation, and if you do, destroy that card, and if you do that, this card gains 1000 DEF.  You can only use this effect of "Elflair - Irene, Wellspring Elf Princess" once per turn.
-	local tp=c:GetControler()
-	local ef=Effect.CreateEffect(c)
-	ef:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-	ef:SetCode(EVENT_PHASE_START+PHASE_DRAW)
-	ef:SetCountLimit(1,5001+EFFECT_COUNT_CODE_DUEL)
-	ef:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
-	ef:SetOperation(function()
-		local tk=Duel.CreateToken(tp,5000)
-		Duel.SendtoDeck(tk,nil,SEQ_DECKBOTTOM,REASON_RULE)
-		c5000.ops(ef,tp)
-	end)
-	Duel.RegisterEffect(ef,tp)
+	c:RegisterSetCardString"Elflair"
+	c:EnableReviveLimit()
+	--mat=2 "Elflair" monsters
+	aux.AddFusionProcFunRep(c,aux.FilterBoolFunction(Card.IsSetCard,"Elflair"),2,true)
+	--This card can attack while in face-up Defense Position. If it does, apply its DEF for damage calculation.
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_DEFENSE_ATTACK)
+	e1:SetValue(1)
+	c:RegisterEffect(e1)
+	--Must first be Special Summoned (from your Extra Deck) by sending the above cards from either field to the GY. (You do not use "Polymerization".)
+	aux.AddContactFusionProcedure(c,s.mfilter,LOCATION_ONFIELD,LOCATION_ONFIELD,Duel.SendtoGrave,REASON_COST)
+	--Once per turn, when a card or effect is activated (Quick Effect): You can remove 1 Elflair counter from anywhere on the field; negate the activation, and if you do, destroy that card, and if you do that, this card gains 1000 DEF. You can only use this effect of "Elflair - Irene, Wellspring Elf Princess" once per turn.
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_QUICK_O)
+	e2:SetCode(EVENT_CHAINING)
+	e2:SetRange(LOCATION_MZONE)
+	e2:HOPT()
+	e2:SetProperty(EFFECT_FLAG_DAMAGE_CAL+EFFECT_FLAG_DAMAGE_STEP)
+	e2:SetCategory(CATEGORY_NEGATE+CATEGORY_DESTROY+CATEGORY_DEFCHANGE)
+	e2:SetCondition(s.negcon)
+	e2:SetCost(s.negcost)
+	e2:SetTarget(s.negtg)
+	e2:SetOperation(s.negop)
+	c:RegisterEffect(e2)
+end
+function s.mfilter(c,fc)
+	return c:IsAbleToGraveAsCost() and (c:IsControler(fc:GetControler()) or c:IsFaceup())
+end
+function s.negcon(e,_,_,_,ev)
+	return not e:GetHandler():IsStatus(STATUS_BATTLE_DESTROYED) and Duel.IsChainNegatable(ev)
+end
+function s.negcost(e,tp,_,_,_,_,_,_,chk)
+	if chk==0 then return Duel.IsCanRemoveCounter(tp,1,1,0x156e,1,REASON_COST) end
+	Duel.RemoveCounter(tp,1,1,0x156e,1,REASON_COST)
+end
+function s.negtg(e,_,eg,_,_,re,_,_,chk)
+	local c=e:GetHandler()
+	if chk==0 then return c:GetFlagEffect(id)<1 end
+	Duel.SetOperationInfo(0,CATEGORY_NEGATE,eg,1,0,0)
+	local rc=re:GetHandler()
+	if rc:IsDestructable() and rc:IsRelateToEffect(re) then
+		Duel.SetOperationInfo(0,CATEGORY_DESTROY,eg,1,0,0)
+	end
+	c:RegisterFlagEffect(id,RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,0,1)
+end
+function s.negop(e,_,eg,_,ev,re)
+	local c=e:GetHandler()
+	if not (Duel.NegateActivation(ev) and re:GetHandler():IsRelateToEffect(re) and Duel.Destroy(eg,REASON_EFFECT)>0
+		and c:IsRelateToChain() and c:IsFaceup()) then return end
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_UPDATE_DEFENSE)
+	e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_DISABLE)
+	e1:SetValue(1000)
+	c:RegisterEffect(e1)
 end
