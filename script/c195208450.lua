6b6460d0e03462fb1e4fc1301bd5c51a3adc012f
Lyris12 <lyris89@live.com>
2024-09-16 11:19:38 -0400

    add `Great London Detective Polan`

--- script/c195208450.lua
+++ script/c195208450.lua
@@ -1,20 +1,103 @@
---created by Seth
+--created by Seth, coded by Lyris
 --Great London Detective Polan
 local s,id,o = GetID()
 function s.initial_effect(c)
-	--During the Main Phase, if this card is in your hand (Quick Effect): You can declare 1 card type (Monster, Spell or Trap); reveal the top card of your Deck, and if you do, Special Summon this card if it is a card of that type. If this card is Normal or Special Summoned: Choose 1 "Great London" card from your Deck and place it on top of your Deck. You can declare 1 card type (Monster, Spell, or Trap); reveal the top card of your Deck, and if you do, add it to your hand if it is a card of that type. At the start of the Damage Step, if this card attacks a monster while you control at least 3 "Great London Clue" cards: Destroy that monster, and if you do, it cannot activate its effects during this Battle Phase. You can only use each effect of "Great London Detective Polan" once per turn.
-	local tp = c:GetControler()
-	local e = Effect.CreateEffect(c)
-	e:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-	e:SetCode(EVENT_PHASE_START+PHASE_DRAW)
-	e:SetCountLimit(1,5001+EFFECT_COUNT_CODE_DUEL)
-	e:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
-	e:SetOperation(function()
-		local tk = Duel.CreateToken(tp,5000)
-		Duel.SendtoDeck(tk,nil,SEQ_DECKBOTTOM,REASON_RULE)
-		local ef = Effect.CreateEffect(tk)
-		tk:RegisterEffect(ef,true)
-		c5000.ops(ef,tp)
-	end)
-	Duel.RegisterEffect(e,tp)
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_QUICK_O)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetRange(LOCATION_HAND)
+	e1:HOPT()
+	e1:SetDescription(1152)
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetCondition(s.spcon)
+	e1:SetTarget(s.sptg)
+	e1:SetOperation(s.spop)
+	c:RegisterEffect(e1)
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_TRIGGER_F)
+	e2:SetCode(EVENT_SUMMON_SUCCESS)
+	e2:HOPT()
+	e2:SetDescription(aux.Stringid(id//10,0))
+	e2:SetOperation(s.sdop)
+	c:RegisterEffect(e2)
+	local e3=e2:Clone()
+	e3:SetCode(EVENT_SPSUMMON_SUCCESS)
+	c:RegisterEffect(e3)
+	local e4=Effect.CreateEffect(c)
+	e4:SetType(EFFECT_TYPE_IGNITION)
+	e4:SetRange(LOCATION_MZONE)
+	e4:HOPT()
+	e4:SetDescription(1190)
+	e4:SetCategory(CATEGORY_SEARCH+CATEGORY_TOHAND)
+	e4:SetTarget(s.thtg)
+	e4:SetOperation(s.thop)
+	c:RegisterEffect(e4)
+	local e5=Effect.CreateEffect(c)
+	e5:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+	e5:SetCode(EVENT_BATTLE_START)
+	e5:HOPT()
+	e5:SetCategory(CATEGORY_DESTROY)
+	e5:SetCondition(s.descon)
+	e5:SetTarget(s.destg)
+	e5:SetOperation(s.desop)
+	c:RegisterEffect(e5)
+end
+function s.spcon()
+	local ph=Duel.GetCurrentPhase()
+	return ph==PHASE_MAIN1 or ph==PHASE_MAIN2
+end
+function s.sptg(e,tp,_,_,_,_,_,_,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false)
+		and Duel.GetFieldGroupCount(tp,LOCATION_DECK,0)>0 end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_CARDTYPE)
+	e:SetLabel(Duel.AnnounceType(tp))
+end
+function s.spop(e,tp)
+	if Duel.GetFieldGroupCount(tp,0,LOCATION_DECK)<1 then return end
+	local tc=Duel.GetDecktopGroup(1-tp,1):GetFirst()
+	Duel.ConfirmDecktop(1-tp,1)
+	if not tc:IsType(1<<e:GetLabel()) or Duel.GetLocationCount(tp,LOCATION_MZONE)<1 then return end
+	Duel.SpecialSummon(e:GetHandler(),0,tp,tp,false,false,POS_FACEUP)
+end
+function s.sdop(e,tp)
+	Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(id//10,0))
+	local tc=Duel.SelectMatchingCard(tp,Card.IsSetCard,tp,LOCATION_DECK,0,1,1,nil,0xd3f):GetFirst()
+	if not tc then return end
+	Duel.ShuffleDeck(tp)
+	Duel.MoveSequence(tc,SEQ_DECKTOP)
+	Duel.ConfirmDecktop(tp,1)
+end
+function s.thtg(e,tp,_,_,_,_,_,_,chk)
+	if chk==0 then return Duel.GetFieldGroupCount(tp,LOCATION_DECK,0)>0 end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_CARDTYPE)
+	e:SetLabel(Duel.AnnounceType(tp))
+end
+function s.thop(e,tp)
+	if Duel.GetFieldGroupCount(tp,0,LOCATION_DECK)<1 then return end
+	local tc=Duel.GetDecktopGroup(1-tp,1):GetFirst()
+	Duel.ConfirmDecktop(1-tp,1)
+	if tc:IsType(1<<e:GetLabel()) then Duel.SendtoHand(tc,nil,REASON_EFFECT) end
+end
+function s.filter(c)
+	return c:IsFaceup() and c:IsSetCard(0x1d3f)
+end
+function s.descon(e,tp)
+	local bc=e:GetHandler():GetBattleTarget()
+	return bc and bc:IsRelateToBattle()
+		and Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_ONFIELD,0,3,nil)
+end
+function s.destg(e,_,_,_,_,_,_,_,,chk)
+	if chk==0 then return true end
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,e:GetHandler():GetBattleTarget(),1,0,0)
+end
+function s.desop(e,tp)
+	local c=e:GetHandler()
+	local bc=c:GetBattleTarget()
+	if not (bc and bc:IsRelateToBattle()) or Duel.Destroy(bc,REASON_EFFECT)<1 then return end
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_CANNOT_TRIGGER)
+	e1:SetReset(RESET_EVENT+RESETS_STANDAR+RESET_PHASE+PHASE_BATTLE)
+	bc:RegisterEffect(e1)
 end
