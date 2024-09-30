8276e7d5f01dbe2a561b43c42558d80650414181
Lyris12 <lyris89@live.com>
2024-09-30 06:02:53 -0400

    add
    `Kronologistics Jumpdrive`
    `Kronologistics Gateway`
    + cleanup

--- script/c212111803.lua
+++ script/c212111803.lua
@@ -2,8 +2,12 @@
 --Kronologistics Dicetron Prime
 local s,id,o = GetID()
 function s.initial_effect(c)
-	--U-If this card is Engaged while you control "The City of Belgrade": You can roll a six-sided die, then gain Energy equal to the result. [0]: Roll a six-sided die, then gain or lose Energy equal to the result. ▼ [-18]: Roll six-sided dice, equal to the number of cards your opponent controls, then destroy 1 card your opponent controls for every die that rolled a 4, 5, or 6. ★ [OD]: Add 1 "The City of Belgrade" from your Deck to your hand.
-	--M-If this card is Drive Summoned while you control "The City of Belgrade": You can roll a six-sided die, then add 1 "The City of Belgrade", or 1 card that specifically lists it in its text, from your Deck to your hand, or if the die result was 4, you can add 1 Level 4 or lower Drive monster from your Deck to your hand, instead. You can only use this effect of "Kronologistics Dicetron Prime" once per turn. If a Drive monster you control battles: You can roll a six-sided die, then if the result is 1 or 6, your monster cannot be destroyed by that battle.
+	--U-If this card is Engaged while you control "The City of Belgrade": You can roll a six-sided die, then gain Energy equal to the result.
+	--[0]: Roll a six-sided die, then gain or lose Energy equal to the result.
+	--[-18]: Roll six-sided dice, equal to the number of cards your opponent controls, then destroy 1 card your opponent controls for every die that rolled a 4, 5, or 6.
+	--[OD]: Add 1 "The City of Belgrade" from your Deck to your hand.
+	--M-If this card is Drive Summoned while you control "The City of Belgrade": You can roll a six-sided die, then add 1 "The City of Belgrade", or 1 card that specifically lists it in its text, from your Deck to your hand, or if the die result was 4, you can add 1 Level 4 or lower Drive monster from your Deck to your hand, instead. You can only use this effect of "Kronologistics Dicetron Prime" once per turn.
+	--If a Drive monster you control battles: You can roll a six-sided die, then if the result is 1 or 6, your monster cannot be destroyed by that battle.
 	local tp = c:GetControler()
 	local e = Effect.CreateEffect(c)
 	e:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
